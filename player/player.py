6c9c245948f0f2f93563b64f0aaa7e5e1630b44c
Matt Ihlenfield <mtihlenfield@gmail.com>
2016-10-08 17:59:26 -0400

    Trying to get it working

--- player/player.py
+++ player/player.py
@@ -7,48 +7,68 @@ import pika
 
 class Player:
 
-    def __init__():
-        pygame.init()
+    def __init__(self):
         pygame.mixer.init()
         self.paused = False
         self.playing = False
 
-    def play(song_loc):
-        pygame.mixer.music.load(file)
+    def play(self, song_loc):
+        print("Playing song")
+        pygame.mixer.music.load(song_loc)
         pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            continue
         self.playing = True
 
-    def pause():
+    def pause(self):
         self.paused = True
         pygame.mixer.music.pause()
 
-    def unpause():
+    def unpause(self):
         self.pause = False
         pygame.mixer.music.unpause()
 
-    def stop():
+    def stop(self):
         self.pause = False
         self.playing = False
         pygame.mixer.stop()
 
-    def is_playing():
-        return pygame.mixer.get_busy()
+    def is_playing(self):
+        busy = pygame.mixer.music.get_busy()
+        print("busy: ", busy)
+        return busy
 
 
 class Slave:
 
-    def __init__():
+    def __init__(self):
         self.player = Player()
+        self.thread = None
 
-    def listen():
-        pass
+    def listen(ch, method, properties, body):
+        print(" [x] Received %r" % body)
 
     def notify():
         pass
 
 
 def main():
-    pass
+    """ slave = Slave()
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='commands')
+    channel.basic_consume(slave.listen, queue='commands', no_ack=True)
+    channel.start_consuming()
+
+    """
+
+    pygame.mixer.init()
+    pygame.mixer.music.load("test.mp3")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        continue
+
 
 if __name__ == "__main__":
     main()
