8bfb6294c591c3af1735e5e9fa5fa3e342572c5e
Matt Ihlenfield <mtihlenfield@gmail.com>
2016-10-10 18:21:13 +0000

    Split Player into a QueueListener and Player class. Had QueueListener stop a playing thread if a new song is requested while another thread is running.

--- player/player.py
+++ player/player.py
@@ -6,97 +6,125 @@ import threading
 import json
 import os
 
-"""
-    Commands:
-        request:
-            - uri
-        stop
-        pause
-
-    Replies sent back:
-        done
-"""
 
 # TODO check if thread is running before starting up a new thread
 # TODO setup logging
 # TODO write unit tests
 
+""" Constants """
 MUSIC_FOLDER = os.path.join(os.path.expanduser("~"), "music")
-
-COMMAND_REQUEST = "request"
-COMMAND_STOP = "stop"
-COMMAND_PAUSE = "pause"
+COMMAND_QUEUE = "command_queue"
+NOTIFY_QUEUE = "song_complete_queue"
+QUEUE_HOST = "localhost"
 
 
 class Player:
+    def __init__(self, music_folder):
+        self.music_folder = music_folder
+        self._instance = vlc.Instance()
+        self._player = self._instance.media_player_new()
 
-    def __init__(self):
-        self.instance = vlc.Instance()
-        self.player = self.instance.media_player_new()
-        self.callback = None
+    def play(self, uri):
+        local_file = os.path.join(self.music_folder, uri)
+        if (os.path.isfile(local_file)):
+            media = self._instance.media_new(local_file)
+        else:
+            media = self._instance.media_new(uri)
+
+        self._player.set_media(media)
+        self._player.play()
+
+    def stop(self):
+        self._player.stop()
+
+    def pause(self):
+        self._player.pause()
+
+    def is_playing(self):
+        return (self._player.get_state() == vlc.State.Playing or
+                self._player.get_state() == vlc.State.Opening)
+
+    def is_paused(self):
+        return (self._player.get_state() == vlc.Paused)
+
+    def is_running(self):
+        return (self._player.get_state() == vlc.State.Playing or
+                self._player.get_state() == vlc.State.Opening or
+                self._player.get_state() == vlc.Paused)
+
+
+class QueueListener:
+
+    COMMAND_REQUEST = "request"
+    COMMAND_STOP = "stop"
+    COMMAND_PAUSE = "pause"
+
+    def __init__(self, song_complete_callback, music_folder):
+        self.song_complete_callback = song_complete_callback
+        self._player = Player(music_folder)
 
     def _play(self, uri):
+
+        if self._player.is_playing or self._player.is_paused():
+            self._player.stop()
+
         def run():
-            local_file = os.path.join(MUSIC_FOLDER, uri)
-            if (os.path.isfile(local_file)):
-                media = self.instance.media_new(local_file)
-            else:
-                media = self.instance.media_new(uri)
+            print("Starting player thread for: ", uri)
+            self._player.play(uri)
 
-            self.player.set_media(media)
-            self.player.play()
-
-            while self.player.get_state() == vlc.State.Playing or self.player.get_state() == vlc.State.Opening:
+            while self._player.is_playing or self._player.is_paused():
                 time.sleep(1)
                 continue
 
-            self.callback()
+            self.song_complete_callback()
+            print("Player thread for ", uri, " stopped.")
 
         thread = threading.Thread(target=run)
         thread.start()
 
     def _pause(self):
-        self.player.pause()
+        self._player.pause()
 
     def _stop(self):
-        self.player.stop()
-
-    def set_callback(self, callback):
-        self.callback = callback
+        self._player.stop()
 
     def listen(self, ch, method, properties, body):
-        print("Received: ", body)
-
         content = json.loads(str(body, "utf-8"))
         command_type = content["command_type"]
-        if command_type == COMMAND_REQUEST:
+
+        if command_type == QueueListener.COMMAND_REQUEST:
             print("playing: ", content)
-            self._play(content["song"])
-        elif command_type == COMMAND_PAUSE:
-            self._pause()
-        elif command_type == COMMAND_STOP:
-            self._stop()
+            self._player.play(content["song"])
+        elif command_type == QueueListener.COMMAND_PAUSE:
+            print("pausing")
+            self._player.pause()
+        elif command_type == QueueListener.COMMAND_STOP:
+            print("stopping")
+            self._player.stop()
         else:
-            raise Exception("You fucked up")
+            raise Exception("Invalid command")
 
 
 def main():
+    print("Starting up...")
 
     def notify():
-        print("notifying")
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        print("Player thread ended. Notifying.")
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=QUEUE_HOST))
         channel = connection.channel()
-        channel.queue_declare(queue='song_complete_queue', durable=False)
-        channel.basic_publish(exchange='', routing_key='song_complete_queue', body='done')
+        channel.queue_declare(queue=NOTIFY_QUEUE, durable=False)
+        channel.basic_publish(exchange='', routing_key=NOTIFY_QUEUE, body='done')
 
-    player = Player()
-    player.set_callback(notify)
+    listener = QueueListener(notify, MUSIC_FOLDER)
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    print("Establishing queue connections...")
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=QUEUE_HOST))
     channel = connection.channel()
-    channel.queue_declare(queue='command_queue', durable=False)
+    channel.queue_declare(queue=COMMAND_QUEUE, durable=False)
 
-    channel.basic_consume(player.listen, queue='command_queue', no_ack=True)
+    channel.basic_consume(listener.listen, queue=COMMAND_QUEUE, no_ack=True)
+
+    print("Waiting for commands on queue '" + COMMAND_QUEUE + "'...")
     channel.start_consuming()
 
 
